6c32da50ae96047188ac0b016a712e6d078ee9ea
mrbrndk <60064167+mrbrndk@users.noreply.github.com>
2021-10-28 15:15:19 +0300

    support for multiple sites

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
+import re
 import requests as req
 import time
-
 import telebot
 from bs4 import BeautifulSoup
 
-url = os.environ.get('URL')
+urls = os.environ.get('URLS')
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'}
 timeout = 5
 check_interval = int(os.environ.get('CHECK_INTERVAL'))
@@ -13,50 +13,61 @@ check_interval = int(os.environ.get('CHECK_INTERVAL'))
 token = os.environ.get('TOKEN')
 chat_id = os.environ.get('CHAT_ID')
 
-last_date_modified = ''
-last_page_text = ''
-
 
 def main():
     bot = telebot.TeleBot(token)
+    url_list = urls.split(";")
+    last_data = list()
+    for url in url_list:
+        last_data.append({'url': url, 'text': '', 'date': ''})
 
     while True:
-        try:
-            page = get_page()
-            result = check_data(page)
-            if result is not None:
-                if isinstance(result, list):
-                    result = '\n'.join(result)
-                result = 'Text has been changed:\n' + result
-                bot.send_message(chat_id, result)
-                print(result)
+        for data in last_data:
+            try:
+                page = get_page(data['url'])
+                result, data['text'], data['date'] = check_data(page, data['text'], data['date'])
+                if result is not None:
+                    if isinstance(result, list):
+                        result = '\n'.join(result)
+                    result = 'Text has been changed on ' + data['url'] + ' :\n' + result
+                    bot.send_message(chat_id, result)
+                    print(result)
 
-        except Exception as e:
-            print(e)
+            except Exception as e:
+                print(e)
 
         time.sleep(check_interval)
 
 
-def get_page():
+def get_page(url):
     r = req.get(url, headers=headers, timeout=timeout)
     if r.status_code == 200:
         soup = BeautifulSoup(r.text, "html.parser")
-        return soup.find('div', {'class': 'part2-item'})
+        page = soup.find('div', {'class': 'part2-item'})
+        if page is not None:
+            return page
+
+        regex = re.compile('"<div class=\'ballon-point\'>.+?"')
+        page = regex.findall(r.text)
+        page = '\n'.join(page)
+        return BeautifulSoup(page, "html.parser")
     else:
         return None
 
 
 def get_date_modified(page_text):
-    return page_text.find('span', {'class': 'itemDateModified'}).string.strip()
+    result = page_text.find('span', {'class': 'itemDateModified'})
+    if result is not None:
+        return result.string.strip()
+    else:
+        return None
 
 
-def check_data(page):
-    global last_date_modified, last_page_text
-
+def check_data(page, last_page_text, last_date_modified):
     if page is not None:
         date_modified = get_date_modified(page)
 
-        if last_date_modified != date_modified:
+        if last_date_modified != date_modified or date_modified is None:
             last_date_modified = date_modified
 
             if last_page_text == '':
@@ -64,8 +75,8 @@ def check_data(page):
             else:
                 result = compare_text(last_page_text, page.text)
                 last_page_text = page.text
-                return result
-    return None
+                return [result, last_page_text, last_date_modified]
+    return [None, last_page_text, last_date_modified]
 
 
 def compare_text(old_text, new_text):
@@ -90,7 +101,10 @@ def compare_text(old_text, new_text):
         for n in new_text:
             result.append(f'+ {n}')
 
-        return result
+        if not result:  # пуст
+            return None
+        else:
+            return result
 
     except Exception as e:
         return str(e)
